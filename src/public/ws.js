f71ee31af04f1aac9f25c12a662261995bddca02
Snail-Chat <88849681+snailychat@users.noreply.github.com>
2021-09-21 13:57:11 -0400

    maybe

--- src/public/ws.js
+++ src/public/ws.js
@@ -1,6 +1,7 @@
 const ws_uri = `ws://${window.location.host}/`;
 const ws = new WebSocket(ws_uri);
 
+document.ws = ws;
 ws.onopen = () => {
     document.back_connection = ws;
 };
@@ -14,9 +15,27 @@ ws.onmessage = ({data}) => {
 function reset () {
     document.getElementById('chat-send').value = '';
 }
+document.reset = reset;
+document.send = () => {
+    const textare = document.getElementById('chat-send');
+    let content = textare.value;
+    content = content.split('\n').join('');
+    if(!content) return reset();
+    //if(!value || value == '') return;
+
+    if(!document.username) ask();
+    document.addMessage(document.username, content, true);
+
+    console.log(document.username, content)
+    document.ws.send(JSON.stringify({
+        username: document.username,
+        content
+    }));
+}
 document.addEventListener('keydown', ({key}) => {
     if(key != 'Enter') return;
-    const textare = document.getElementById('chat-send');
+    document.send();
+    /*const textare = document.getElementById('chat-send');
     let content = textare.value;
     content = content.split('\n').join('');
     if(!content) return reset();
@@ -29,7 +48,7 @@ document.addEventListener('keydown', ({key}) => {
     ws.send(JSON.stringify({
         username: document.username,
         content
-    }));
+    }));*/
 
     reset();
 });
